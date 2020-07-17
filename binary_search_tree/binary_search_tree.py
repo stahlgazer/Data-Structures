b2825b886e923b26c7276c0b8e98b5ba682f7eeb
Gavin S <stahlgavin@gmail.com>
2020-07-16 18:03:06 -0700

    almost complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,11 +9,18 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from stack import Stack
+from queue import Queue
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+
+    def __str__(self):
+        return f"{self.value}"
 
     # Insert the given value into the tree
     def insert(self, value):
@@ -63,9 +70,6 @@ class BSTNode:
             return self.right.get_max()
         # no self.right means its the maximum   
         else: return self.value
-        
-        
-        pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
@@ -77,25 +81,78 @@ class BSTNode:
         else: 
             pass
 
-
-    # Part 2 ----------------------- Part 2
-    # Part 2 ----------------------- Part 2
-    # Part 2 ----------------------- Part 2
-
+# Part 2 -----------------------
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    def in_order_print(self, node=None):
+        # if the current node is None
+        # we know we've reached the end of a recursion
+        # (base case) we want to return
+        if self is None:
+            return
+
+        # check if we can "move left"
+        if self.left is not None:
+            self.left.in_order_print()
+
+        # visit the node by printing its value
+        print(self.value)
+
+        # check if we can "move right"
+        if self.right is not None:
+            self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
+    def bft_print(self, node=None):
+        # You should import the queue class from earlier in the
+        # week and use that class to implement this method
+        # Use a queue to form a "line" 
+        # for the nodes to "get in"
+        # start by placing the root in the queue:
+        line = Queue()
+        line.enqueue(self)
+        # need a while loop to iterate:
+        # what are we checking in the while statement?
+        # while length of queue is greater than 0
+        while len(line) > 0:
+            # dequeue item from front of queue
+            removed = line.dequeue()
+            # print that item
+            print(removed.value)
+            # place current item's left node in queue if not None
+            if removed.left is not None:
+                line.enqueue(removed.left)
+            # place current item's right node in queue if not None
+            if removed.right is not None:
+                line.enqueue(removed.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
+    def dft_print(self, node=None):
         pass
+        # initialize an empty stack
+        stacks = Stack()
+        # push the root node onto the stack
+        stack.push(self)
+        # need a while loop to manager our iteration
+        # if stack is not empty enter the while loop
+        while len(stack) > 0:
+            # pop top item off the stack
+            current = stack.pop()
+            # print that item's value
+            print(current)
+
+            # if there is a right subtree
+            if current.right:
+                # push right item onto the stack
+                stack.push(current.right)
+                
+            # if there is a left subtree
+            if current.left:
+                # push left item onto the stack
+                stack.push(current.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -110,6 +167,13 @@ class BSTNode:
 
 
 tree = BSTNode(1)
+tree.insert(7)
 tree.insert(2)
+tree.insert(3)
+tree.insert(4)
+tree.insert(5)
+tree.in_order_print()
 print(tree.contains(2))
-print(tree.value)
+tree.dft_print()
+print(tree.contains(1))
+tree.bft_print()
--- binary_search_tree/queue.py
+++ binary_search_tree/queue.py
@@ -1,52 +1,54 @@
 """
-A stack is a data structure whose primary purpose is to store and
-return elements in Last In First Out order. 
+A queue is a data structure whose primary purpose is to store and
+return elements in First In First Out order. 
 
-1. Implement the Stack class using an array as the underlying storage structure.
-   Make sure the Stack tests pass.
-2. Re-implement the Stack class, this time using the linked list implementation
+1. Implement the Queue class using an array as the underlying storage structure.
+   Make sure the Queue tests pass.
+2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
-   Make sure the Stack tests pass.
+   Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
-   implementing a Stack?
+   implementing a Queue?
+   
+Stretch: What if you could only use instances of your Stack class to implement the Queue?
+         What would that look like? How many Stacks would you need? Try it!
 """
-
 # from singly_linked_list import Node, LinkedList 
 
 # Array/List Version
-class Stack:
+class Queue:
     def __init__(self):
         self.size = 0
         # self.storage = ?
         self.storage = []
-
+    
     def __len__(self):
         return len(self.storage)
 
-    def push(self, value):
+    def enqueue(self, value):
         self.storage.append(value)
         self.size = len(self.storage)
 
-    def pop(self):
+    def dequeue(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.pop(self.size)
+            return self.storage.pop(0)
 
-# class Stack:
+# class Queue:
 #     def __init__(self):
 #         self.size = 0
 #         # self.storage = ?
 #         self.storage = LinkedList()
-
+    
 #     def __len__(self):
 #         return self.size
 
-#     def push(self, value):
+#     def enqueue(self, value):
 #         value = self.storage.add_to_tail(value)
 #         self.size += 1
 #         return value
-
-#     def pop(self):
+        
+#     def dequeue(self):
 #         if self.size > 0:
 #             self.size -= 1
-#             return self.storage.remove_tail()
+#             return self.storage.remove_head()
